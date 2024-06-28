85d88d9c543da09e96e7808966587e5c87c1c20f
Serhat Karabulut <serhat.karabulut@trendyol.com>
2024-06-28 21:12:48 +0300

    feat: add index and delete actions

--- elasticsearch/action.go
+++ elasticsearch/action.go
@@ -1,3 +1,33 @@
 package elasticsearch
 
-type Action struct{}
+type ActionType string
+
+const (
+	Index  ActionType = "Index"
+	Delete ActionType = "Delete"
+)
+
+type Action struct {
+	Routing   *string
+	Type      ActionType
+	IndexName string
+	Source    []byte
+	ID        []byte
+}
+
+func NewDeleteAction(key []byte, routing *string) Action {
+	return Action{
+		ID:      key,
+		Routing: routing,
+		Type:    Delete,
+	}
+}
+
+func NewIndexAction(key []byte, source []byte, routing *string) Action {
+	return Action{
+		ID:      key,
+		Routing: routing,
+		Source:  source,
+		Type:    Index,
+	}
+}
