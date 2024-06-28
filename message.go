423e55a4dc835b2db6e11d59d5e8b9a318b3e6c9
Serhat Karabulut <serhat.karabulut@trendyol.com>
2024-06-28 22:19:06 +0300

    feat: add message fields

--- message.go
+++ message.go
@@ -2,18 +2,62 @@ package cdc
 
 import (
 	"github.com/Trendyol/go-pq-cdc/pq/message/format"
+	"github.com/elastic/go-elasticsearch/v7"
+	"time"
 )
 
-type Message struct{}
+type Message struct {
+	ElasticsearchClient *elasticsearch.Client
+	EventTime           time.Time
+	TableName           string
+	TableNamespace      string
 
-func NewInsertMessage(m *format.Insert) Message {
-	return Message{}
+	OldData map[string]any
+	NewData map[string]any
+
+	IsInsert bool
+	IsUpdate bool
+	IsDelete bool
 }
 
-func NewUpdateMessage(m *format.Update) Message {
-	return Message{}
+func NewInsertMessage(esClient *elasticsearch.Client, m *format.Insert) Message {
+	return Message{
+		ElasticsearchClient: esClient,
+		EventTime:           m.MessageTime,
+		TableName:           m.TableName,
+		TableNamespace:      m.TableNamespace,
+		OldData:             nil,
+		NewData:             m.Decoded,
+		IsInsert:            true,
+		IsUpdate:            false,
+		IsDelete:            false,
+	}
 }
 
-func NewDeleteMessage(m *format.Delete) Message {
-	return Message{}
+func NewUpdateMessage(esClient *elasticsearch.Client, m *format.Update) Message {
+	return Message{
+		ElasticsearchClient: esClient,
+		EventTime:           m.MessageTime,
+		TableName:           m.TableName,
+		TableNamespace:      m.TableNamespace,
+		OldData:             m.OldDecoded,
+		NewData:             m.NewDecoded,
+		IsInsert:            false,
+		IsUpdate:            true,
+		IsDelete:            false,
+	}
+}
+
+func NewDeleteMessage(esClient *elasticsearch.Client, m *format.Delete) Message {
+	return Message{
+		ElasticsearchClient: esClient,
+		EventTime:           m.MessageTime,
+		TableName:           m.TableName,
+		TableNamespace:      m.TableNamespace,
+		OldData:             m.OldDecoded,
+		NewData:             nil,
+		IsInsert:            false,
+		IsUpdate:            false,
+		IsDelete:            true,
+	}
 }
