d8efcb6075f2fde721f76993d2d80e781e05368d
Mehmet Firat Komurcu <mehmetfiratkomurcu@hotmail.com>
2025-05-27 13:47:27 +0300

    feat: add painless script update action
    * #31 add painless script feat.
    
    * #12 add painless script feat.
    
    * 12 add painless script feat.
    
    * #12 refactor: reorder fields in Action and Script structs for consistency
    
    * create go.sum file
    
    ---------
    
    Co-authored-by: firat.komurcu <firat.komurcu@trendyol.com>

--- elasticsearch/action.go
+++ elasticsearch/action.go
@@ -1,18 +1,26 @@
 package elasticsearch
 
+import "encoding/json"
+
 type ActionType string
 
 const (
-	Index  ActionType = "Index"
-	Delete ActionType = "Delete"
+	Index        ActionType = "Index"
+	Delete       ActionType = "Delete"
+	ScriptUpdate ActionType = "ScriptUpdate"
 )
 
 type Action struct {
 	Routing   *string
-	Type      ActionType
 	IndexName string
+	Type      ActionType
 	Source    []byte
 	ID        []byte
+}
+
+type Script struct {
+	Params map[string]interface{} `json:"params,omitempty"`
+	Source string                 `json:"source"`
 }
 
 func NewDeleteAction(key []byte, routing *string) Action {
@@ -29,5 +37,15 @@ func NewIndexAction(key []byte, source []byte, routing *string) Action {
 		Routing: routing,
 		Source:  source,
 		Type:    Index,
+	}
+}
+
+func NewScriptUpdateAction(id []byte, script Script, routing *string) Action {
+	scriptBytes, _ := json.Marshal(script)
+	return Action{
+		ID:      id,
+		Type:    ScriptUpdate,
+		Source:  scriptBytes,
+		Routing: routing,
 	}
 }
